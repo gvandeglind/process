4156c293e97b651e35efa463640587b8c99ae580
Gerard van de Glind <gvdglind@wehkamp.nl>
2015-09-03 21:20:43 +0200

    reordering & grouping

--- src/main/scala/processframework/ProcessStep.scala
+++ src/main/scala/processframework/ProcessStep.scala
@@ -8,12 +8,20 @@ import akka.actor.{ Actor, ActorContext, ActorRef, Props }
 import akka.util.Timeout
 
 trait ProcessStep[S] {
-  implicit def context: ActorContext
-  private[processframework] val promise: Promise[Unit] = Promise[Unit]()
-
   type Execution = S ⇒ Unit
   type UpdateFunction = PartialFunction[Process.Event, S ⇒ S]
   type CommandToEvent = PartialFunction[Any, Process.Event]
+
+  implicit def context: ActorContext
+
+  private[processframework] val promise: Promise[Unit] = Promise[Unit]()
+  private val innerActor = context.actorOf(Props(new Actor {
+    def receive = {
+      case msg if receiveCommand.isDefinedAt(msg) ⇒
+        val event = receiveCommand(msg)
+        context.parent ! event
+    }
+  }))
 
   def execute()(implicit process: ActorRef): Execution
   def receiveCommand: CommandToEvent
@@ -27,22 +35,13 @@ trait ProcessStep[S] {
     markDone()
     state
   }
-  private[processframework] def abort(): Unit = promise.tryFailure(new RuntimeException("Process aborted"))
   final def onComplete(completeFn: ((ActorContext, S)) ⇒ Unit)(implicit executionContext: ExecutionContext, process: ActorRef): Unit =
     promise.future.foreach { _ ⇒ process ! PersistentProcess.Perform(completeFn) }
-
   final def onCompleteAsync(completeFn: ⇒ Unit)(implicit executionContext: ExecutionContext): Unit = promise.future.foreach(_ ⇒ completeFn)
-
   final def ~>(next: ProcessStep[S]*)(implicit context: ActorContext): ProcessStep[S] = new Chain(this, next: _*)
 
+  private[processframework] def abort(): Unit = promise.tryFailure(new RuntimeException("Process aborted"))
   private[processframework] def run()(implicit process: ActorRef, executionContext: ExecutionContext, classTag: ClassTag[S]): Future[Unit] = runImpl
-  private val innerActor = context.actorOf(Props(new Actor {
-    def receive = {
-      case msg if receiveCommand.isDefinedAt(msg) ⇒
-        val event = receiveCommand(msg)
-        context.parent ! event
-    }
-  }))
   private[processframework] def handleUpdateState: UpdateFunction = if (isCompleted) PartialFunction.empty[Process.Event, S ⇒ S] else updateState
   private[processframework] def handleReceiveCommand: CommandToEvent = if (isCompleted) PartialFunction.empty[Any, Process.Event] else receiveCommand
   private[processframework] def executeWithPossibleRetry()(implicit process: ActorRef): Execution = { state ⇒
